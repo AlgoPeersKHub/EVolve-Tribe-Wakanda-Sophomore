dec6b3c6ab59bb6062b2fe571df6dde07b6d4dd7
navpayal <137693630+navpayal@users.noreply.github.com>
2024-01-30 15:05:14 +0000

    Update battery_management_system.py
    This is the code for the battery management system. It uses the ADS1115 Analog to Digital Converter and reads the voltage values of Analog pin A0. 
    
    Unfortunately I do not have access to an ADS1115 to test the code so it will have to be verified by testing it as per the instructions before use.

--- battery_management_system.py
+++ battery_management_system.py
@@ -1,16 +1,39 @@
-from machine import ADC
-from time import sleep
+import utime
 
-analogIn = ADC(7)
-minutes = 0
-seconds = 0
+from machine import I2C, Pin
 
-while True:  
-    sensorValue = analogIn.read_u16()
-    voltage = sensorValue * (3.3 / 65535)
-    writeLine(str(minutes) + "," + str(seconds) + "," + str(voltage))
-    seconds += 1
-    if (seconds == 60):
-        seconds = 0
-        minutes += 1
-    sleep(1)
+dev = I2C(1, freq = 400000, scl=Pin(15), sda=Pin(14))
+devices = dev.scan()
+
+for device in devices:
+    print(device)
+
+address = 72
+
+def readConfig():
+    dev.writeto(address, bytearray([1]))
+    result = dev.readfrom(address,2)
+    return result[0]<<8 | result[1]
+
+def readValue():
+    dev.writeto(address, bytearray([0]))
+    result = dev.readfrom(address,2)
+
+    config = readConfig()
+    config &= ~(7<<12) & ~(7<<9)
+    config |= (4<<12) | (1<<9) | (1<<15)
+    
+    config = [ int(config>>i & 0xff) for i in (8,0)]
+
+    dev.writeto(address, bytearray([1] + config))
+
+    return result[0]<<8 | result[1]
+
+print(bin(readConfig()))
+
+whileTrue:
+    val = readValue()
+    print("val = ", val, "\tVolts = ", (3.3/26250) * val)
+    utime.sleep(0.1)
+
+#from https://www.youtube.com/watch?v=HiuNYLDvY9k&t=334s
